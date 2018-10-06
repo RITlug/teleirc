248d733ee61620f569270d13bac8ae29fe2cfd21
Seth Hendrick <seth@shendrick.net>
2018-10-06 14:22:57 -0400

    Bind *all* the things (...thanks node...).

--- tests/JoinHandlerTests.js
+++ tests/JoinHandlerTests.js
@@ -2,6 +2,33 @@
 
 const JoinHandler = require("../lib/IrcHandlers/JoinHandler");
 
+class ActionClass {
+    constructor(action) {
+        this._action = action;
+    }
+
+    DoAction(message) {
+        this._action(message);
+    }
+}
+
+/**
+ * Ensures that we can pass in a function pointer to the class just fine.
+ */
+exports.JoinHandler_FunctionPointerClass = function(assert) {
+    var message = undefined;
+
+    let actionClass = new ActionClass( (msg) => {message=msg;});
+    let uut = new JoinHandler(true, actionClass.DoAction.bind(actionClass));
+
+    uut.ReportJoin("#channel", "user");
+
+    // Disabled, message should remain undefined.
+    assert.strictEqual(message, "user has joined #channel channel.");
+
+    assert.done();
+}
+
 /**
  * Ensures that if the handler is disabled,
  * nothing happens.
