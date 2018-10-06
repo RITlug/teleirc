83a6ca94827bc7732cdc7c94339f755cae3d53df
Seth Hendrick <seth@shendrick.net>
2018-10-06 15:48:50 -0400

    Rename IrcHandlers so they have "Irc" in front of their names, so there is no mistake.

--- tests/KickHandlerTests.js
+++ tests/KickHandlerTests.js
@@ -1,15 +1,15 @@
 'use strict';
 
-const KickHandler = require("../lib/IrcHandlers/KickHandler");
+const IrcKickHandler = require("../lib/IrcHandlers/KickHandler");
 
 /**
  * Ensures that if the handler is disabled,
  * nothing happens.
  */
-exports.KickHandler_DisabledTest = function(assert) {
+exports.IrcKickHandler_DisabledTest = function(assert) {
     var message = undefined;
 
-    let uut = new KickHandler(false, (msg) => {message = msg;});
+    let uut = new IrcKickHandler(false, (msg) => {message = msg;});
 
     uut.ReportKick("#channel", "kickeduser", "opuser", "reason");
 
@@ -24,10 +24,10 @@ exports.KickHandler_DisabledTest = function(assert) {
  * its callback is activated.
  * This test has a reason defined.
  */
-exports.KickHandler_EnabledWithReasonTest = function(assert) {
+exports.IrcKickHandler_EnabledWithReasonTest = function(assert) {
     var message = undefined;
 
-    let uut = new KickHandler(true, (msg) => {message = msg;});
+    let uut = new IrcKickHandler(true, (msg) => {message = msg;});
 
     uut.ReportKick("#channel", "kickeduser", "opuser", "reason");
 
@@ -42,10 +42,10 @@ exports.KickHandler_EnabledWithReasonTest = function(assert) {
  * its callback is activated.
  * This test does not have a reason defined, so it should default.
  */
-exports.KickHandler_EnabledWithOutReasonTest = function(assert) {
+exports.IrcKickHandler_EnabledWithOutReasonTest = function(assert) {
     var message = undefined;
 
-    let uut = new KickHandler(true, (msg) => {message = msg;});
+    let uut = new IrcKickHandler(true, (msg) => {message = msg;});
 
     uut.ReportKick("#channel", "kickeduser", "opuser", undefined);
 
