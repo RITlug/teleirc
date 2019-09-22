c3e553eb8f0b2decd32f4e341d65803b8db46d71
Nicholas Jones <dojoman19@gmail.com>
2019-09-22 11:17:10 -0400

    Build out config parsing
    This builds out the initial .env file parsing, and enables the user to choose a path location.
    Closes #174

---
+++ cmd/teleirc.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/ritlug/teleirc/internal"
+)
+
+func main() {
+	settings, err := internal.LoadConfig("../env.example")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(settings)
+	}
+}
