9cec8d29240b9477c393a72743394d28cd2b1ad0
Tim Zabel <Tjzabel21@gmail.com>
2019-09-23 21:38:30 -0400

    Add skeleton IRC and Telegram handlers.

---
+++ internal/handlers/irc/irc.go
@@ -0,0 +1,8 @@
+package irc
+
+/*
+StartIrcBot instantiates an IRC bot object given its IRC Bot settings
+*/
+func StartIrcBot() (string, error) {
+	return "Hello from IRC!", nil
+}
---
+++ internal/handlers/telegram/telegram.go
@@ -0,0 +1,8 @@
+package telegram
+
+/*
+StartTelegramBot: Start up Telegram bot
+*/
+func StartTelegramBot() (string, error) {
+	return "Hello from Telegram!", nil
+}
