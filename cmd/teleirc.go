67061d8fd4d8ab088f6f42a7b41db3a35245080a
Tim Zabel <Tjzabel21@gmail.com>
2019-09-23 21:39:30 -0400

    Add support for custom env path. Build out initial bot handlers.

--- cmd/teleirc.go
+++ cmd/teleirc.go
@@ -1,15 +1,52 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/ritlug/teleirc/internal"
+	"github.com/ritlug/teleirc/internal/handlers/irc"
+	tg "github.com/ritlug/teleirc/internal/handlers/telegram"
 )
 
+/*
+startIrc: Start up IRC bot
+*/
+func startIrc() {
+	// start up IRC bot
+	ircbot, err := irc.StartIrcBot()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(ircbot)
+}
+
+/*
+startTelegram: Start up Telegram bot
+*/
+func startTelegram() {
+	// start up Telegram bot
+	tgbot, err := tg.StartTelegramBot()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(tgbot)
+}
+
 func main() {
-	settings, err := internal.LoadConfig("../env.example")
+	// optional path flag if user does not want .env file in root dir
+	var path string
+	flag.StringVar(&path, "p", "../.env", "Path to .env")
+
+	flag.Parse()
+
+	// TODO: change _ to settings once used
+	_, err := internal.LoadConfig(path)
 	if err != nil {
 		fmt.Println(err)
 	} else {
-		fmt.Println(settings)
+		startIrc()
+		startTelegram()
 	}
 }
