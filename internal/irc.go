877053281be8fec1b56e975e4fc0055410e6bc82
Zedjones <dojoman19@gmail.com>
2019-09-28 09:53:35 -0400

    Added docstrings and renamed Client to IRCClient

--- internal/irc.go
+++ internal/irc.go
@@ -6,12 +6,19 @@ import (
 	"github.com/lrstanley/girc"
 )
 
-type Client struct {
+/*
+IRCClient contains information for our IRC bridge, including the girc Client
+and the IRCSettings that were passed into NewClient
+*/
+type IRCClient struct {
 	*girc.Client
 	Settings IRCSettings
 }
 
-func NewClient(settings *Settings) Client {
+/*
+NewClient returns a new IRCClient based on the provided settings
+*/
+func NewClient(settings *Settings) IRCClient {
 	client := girc.New(girc.Config{
 		Server: settings.IRC.Server,
 		Port:   settings.IRC.Port,
@@ -22,17 +29,21 @@ func NewClient(settings *Settings) Client {
 			Pass: settings.IRC.NickServPassword,
 		},
 	})
-	return Client{client, settings.IRC}
+	return IRCClient{client, settings.IRC}
 }
 
-func (c Client) AddHandlers() {
+/*
+AddHandlers adds handlers for the client struct based on the settings
+that were passed in to NewClient
+*/
+func (c IRCClient) AddHandlers() {
 	c.Handlers.Add(girc.ALL_EVENTS, func(c *girc.Client, e girc.Event) {
 		fmt.Println(e.String())
 	})
 	c.Handlers.Add(girc.CONNECTED, connectHandlerTest(c))
 }
 
-func connectHandlerTest(c Client) func(*girc.Client, girc.Event) {
+func connectHandlerTest(c IRCClient) func(*girc.Client, girc.Event) {
 	return func(gc *girc.Client, e girc.Event) {
 		c.Cmd.Join(c.Settings.Channel)
 	}
