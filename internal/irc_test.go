1dbb6c7579292d530b73034951f201381d004507
Zedjones <dojoman19@gmail.com>
2019-09-28 13:19:20 -0400

    Added test for full new client

--- internal/irc_test.go
+++ internal/irc_test.go
@@ -27,3 +27,28 @@ func TestNewClientBasic(t *testing.T) {
 	assert.Equal(t, client.Settings, ircSettings, "Client settings should be properly set")
 	assert.Equal(t, client.Config, expectedConfig, "girc config should be properly set")
 }
+
+func TestNewClientFull(t *testing.T) {
+	ircSettings := IRCSettings{
+		Server:           "test_server",
+		Port:             1234,
+		BotName:          "test_name",
+		NickServPassword: "test_pass",
+	}
+	client := NewClient(&Settings{IRC: ircSettings})
+	expectedPing, _ := time.ParseDuration("20s")
+	expectedConfig := girc.Config{
+		Server:    "test_server",
+		Port:      1234,
+		Nick:      "test_name",
+		User:      "test_name",
+		PingDelay: expectedPing,
+		SASL: &girc.SASLPlain{
+			User: "test_name",
+			Pass: "test_pass",
+		},
+	}
+	assert.Equal(t, client.Settings, ircSettings, "Client settings should be properly set")
+	assert.Equal(t, client.Config, expectedConfig, "girc config should be properly set")
+
+}
