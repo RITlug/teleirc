a931eb119009bd03df9ad4a3fa8f9f574faafca4
Tim Zabel <Tjzabel21@gmail.com>
2019-09-29 21:53:09 -0400

    Add base logic connecting IRC and Telegram

---
+++ internal/handlers/telegram/telegram.go
@@ -0,0 +1,9 @@
+// Package Telegram handles all Telegram-side logic.
+package telegram
+
+/*
+StartBot instantiates the Telegram bot.
+*/
+func StartBot() (string, error) {
+	return "Hello from Telegram!", nil
+}
