c29008b00ac2349c971b013b8ddf00d7daae0d65
Tim Zabel <Tjzabel21@gmail.com>
2019-09-29 21:53:09 -0400

    Add base logic connecting IRC and Telegram

--- cmd/teleirc.go
+++ cmd/teleirc.go
@@ -1,16 +1,45 @@
+// Package main contains all logic relating to running TeleIRC
 package main
 
 import (
+	"flag"
 	"fmt"
-
 	"github.com/ritlug/teleirc/internal"
+	"github.com/ritlug/teleirc/internal/handlers/irc"
+	tg "github.com/ritlug/teleirc/internal/handlers/telegram"
 )
 
+func startIrc() {
+	ircbot, err := irc.StartBot()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(ircbot)
+}
+
+func startTelegram() {
+	tgbot, err := tg.StartBot()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(tgbot)
+}
+
 func main() {
-	settings, err := internal.LoadConfig("../env.example")
+	// optional path flag if user does not want .env file in root dir
+	var path string
+	flag.StringVar(&path, "p", ".env", "Path to .env")
+
+	flag.Parse()
+
+	// TODO: change _ to settings once used
+	_, err := internal.LoadConfig(path)
 	if err != nil {
 		fmt.Println(err)
 	} else {
-		fmt.Println(settings)
+		startIrc()
+		startTelegram()
 	}
 }
