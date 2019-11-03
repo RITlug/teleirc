a533fcfbc4e59bedf0ca115355fabbf5a58c0730
Justin W. Flory <git@jwf.io>
2019-11-03 16:21:06 -0500

    docs: Support Markdown file type (closes #208)
    This commit adds Markdown support to our documentation. Files that end
    in a `.md` extension will be detected by Sphinx as documentation to
    render in `docs/`. We can write in whatever format we wish.
    
    Closes #208.
    
    Signed-off-by: Justin W. Flory <git@jwf.io>

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,13 @@ import sphinx_rtd_theme
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.imgmath']
+extensions = [
+    'recommonmark',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.todo',
+    'sphinx.ext.imgmath',
+    'sphinx.ext.viewcode',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -42,7 +48,11 @@ templates_path = ['_templates']
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
 
 # The master toctree document.
 master_doc = 'index'
