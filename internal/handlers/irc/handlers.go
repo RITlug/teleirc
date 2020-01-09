216e468bfdb3cff2972a7f222130984f26815fc9
Zedjones <dojoman19@gmail.com>
2020-01-08 20:30:45 -0500

    Added documentation for exported functions

--- internal/handlers/irc/handlers.go
+++ internal/handlers/irc/handlers.go
@@ -4,6 +4,11 @@ import (
 	"github.com/lrstanley/girc"
 )
 
+/*
+Handler specifies a function that handles an IRC event
+In this case, we take an IRC client and return a function that
+handles an IRC event
+*/
 type Handler = func(c Client) func(*girc.Client, girc.Event)
 
 /*
@@ -24,6 +29,9 @@ func privMsgHandler(c Client) func(*girc.Client, girc.Event) {
 	}
 }
 
+/*
+GetHandlerMapping returns a mapping of girc event types to handlers
+*/
 func GetHandlerMapping() map[string]Handler {
 	return map[string]Handler{
 		girc.CONNECTED: connectHandler,
