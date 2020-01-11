f6b9137a32a2701adba8b7040c331f89d429073f
Zedjones <dojoman19@gmail.com>
2020-01-11 17:56:23 -0500

    Added newline to .gitignore and fixed typo

--- internal/handlers/irc/handlers.go
+++ internal/handlers/irc/handlers.go
@@ -14,7 +14,7 @@ handles an IRC event
 type Handler = func(c Client) func(*girc.Client, girc.Event)
 
 /*
-connectHandler return a function to use as the connect handler for girc,
+connectHandler returns a function to use as the connect handler for girc,
 so that the specified channel is joined after the server connection is established
 */
 func connectHandler(c Client) func(*girc.Client, girc.Event) {
