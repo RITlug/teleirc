e5914b99bc5dcd17b930a2468ea7a8218142adc1
10eMyrT <kta7930@rit.edu>
2020-03-11 18:33:08 -0400

    Changed verbose reference by adding the field name

--- cmd/teleirc.go
+++ cmd/teleirc.go
@@ -24,7 +24,7 @@ var (
 func main() {
 	flag.Parse()
 
-	verbose := internal.Debug {DebugLevel:  *flagDebug	}
+	verbose := internal.Debug { DebugLevel:  *flagDebug }
 
 	if *flagVersion {
 		verbose.PrintVersion("Current TeleIRC version: " + version)
