c44e8d6396634125f6d10385828437cf259d3dc6
Tim Zabel <Tjzabel21@gmail.com>
2020-04-02 23:22:28 -0400

    Add Telegram join/part handlers. Add more verbose logging for the contents of unhandled TG updates.

--- internal/handlers/telegram/handler.go
+++ internal/handlers/telegram/handler.go
@@ -21,6 +21,10 @@ func updateHandler(tg *Client, updates tgbotapi.UpdatesChannel) {
 		case u.Message == nil:
 			tg.logger.LogError("Missing message data")
 			continue
+		case u.Message.NewChatMembers != nil:
+			joinHandler(tg, u.Message.NewChatMembers)
+		case u.Message.LeftChatMember != nil:
+			partHandler(tg, u.Message.LeftChatMember)
 		case u.Message.Text != "":
 			tg.logger.LogDebug("messageHandler triggered")
 			messageHandler(tg, u)
@@ -31,7 +35,8 @@ func updateHandler(tg *Client, updates tgbotapi.UpdatesChannel) {
 			tg.logger.LogDebug("documentHandler triggered")
 			documentHandler(tg, u.Message)
 		default:
-			tg.logger.LogWarning("triggered, but message type is currently unsupported")
+			tg.logger.LogWarning("Triggered, but message type is currently unsupported")
+			tg.logger.LogWarning("Unhandled Update:", u)
 			continue
 		}
 	}
@@ -44,6 +49,27 @@ Telegram update into a simple string for IRC.
 func messageHandler(tg *Client, u tgbotapi.Update) {
 	formatted := tg.Settings.Prefix + u.Message.From.UserName +
 		tg.Settings.Suffix + u.Message.Text
+
+	tg.sendToIrc(formatted)
+}
+
+/*
+joinHandler handles when users join the Telegram group
+*/
+func joinHandler(tg *Client, users *[]tgbotapi.User) {
+	for _, user := range *users {
+		username := GetUsername(&user)
+		formatted := username + " has joined the Telegram Group!"
+		tg.sendToIrc(formatted)
+	}
+}
+
+/*
+partHandler handles when users leave the Telegram group
+*/
+func partHandler(tg *Client, user *tgbotapi.User) {
+	username := GetUsername(user)
+	formatted := username + " has left the Telegram Group!"
 
 	tg.sendToIrc(formatted)
 }
@@ -63,14 +89,14 @@ func stickerHandler(tg *Client, u tgbotapi.Update) {
 documentHandler receives a document object from Telegram, and sends
 a notification to IRC.
 */
-func documentHandler(tg *Client, u *tgbotapi.Message) {
+func documentHandler(tg *Client, m *tgbotapi.Message) {
 
-	formatted := u.From.UserName + " shared a file (" + u.Document.MimeType + ")"
+	formatted := m.From.UserName + " shared a file (" + m.Document.MimeType + ")"
 
-	if u.Caption != "" {
-		formatted += " on Telegram with caption: " + "'" + u.Caption + "'."
+	if m.Caption != "" {
+		formatted += " on Telegram with caption: " + "'" + m.Caption + "'."
 	} else {
-		formatted += " on Telegram with title: " + "'" + u.Document.FileName + "'."
+		formatted += " on Telegram with title: " + "'" + m.Document.FileName + "'."
 	}
 
 	tg.sendToIrc(formatted)
