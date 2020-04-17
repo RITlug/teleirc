2cb8e7d0fda220c879d4242eebf0a42abe53c8c3
Tim Zabel <Tjzabel21@gmail.com>
2020-04-17 19:56:47 -0400

    Remove unnecessary else statement from helpers.go

--- internal/handlers/telegram/helpers.go
+++ internal/handlers/telegram/helpers.go
@@ -5,14 +5,12 @@ import (
 )
 
 /*
-GetUsername returns the username of a user. Since usernames are optional
-on Telegram, we first need to check to see if they have one set. If no
-username is set, only the first name is used.
+GetUsername returns the name and username of a user. Since usernames are optional
+on Telegram, we first need to check to see if they have one set.
 */
 func GetUsername(u *tgbotapi.User) string {
 	if u.UserName == "" {
 		return u.FirstName
-	} else {
-		return u.FirstName + " (@" + u.UserName + ")"
 	}
+	return u.FirstName + " (@" + u.UserName + ")"
 }
