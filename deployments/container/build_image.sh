afadee68e37d196643fae41ea54fd49120f8f307
Zedjones <dojoman19@gmail.com>
2020-05-09 20:38:37 -0400

    Added multi-staged build and modified other files

--- deployments/container/build_image.sh
+++ deployments/container/build_image.sh
@@ -1,3 +1,2 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-go build "${DIR}/../../cmd/teleirc.go"
-docker build ${DIR} -t teleirc
+docker build ${DIR}/../../ -f ${DIR}/Dockerfile -t teleirc
