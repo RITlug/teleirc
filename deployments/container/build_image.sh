3f85fd8a4ff8464811b46e013be6d5d4cc9ffd5c
Tim Zabel <Tjzabel21@gmail.com>
2025-04-24 22:49:03 -0400

    Create Stable TeleIRC Dockerfile (#437)
    * Update Dockerfile for go v1.22
    * Update quick start guide to recommend Dockerfile deployments

--- deployments/container/build_image.sh
+++ deployments/container/build_image.sh
@@ -1,2 +1,2 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-docker build ${DIR}/../../ -f ${DIR}/Dockerfile -t teleirc
+docker build ${DIR}/../../ -f ${DIR}/Dockerfile -t teleirc:latest
